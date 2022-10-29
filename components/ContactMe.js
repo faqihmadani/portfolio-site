c9bf6c6d42b90a985b9bd9b5c5bea3dc8919b297
Faqih Madani <faqihfaqih22@gmail.com>
2022-10-29 22:01:41 +0700

    add success page

--- components/ContactMe.js
+++ components/ContactMe.js
@@ -25,7 +25,7 @@ const ContactMe = () => {
 
                     </div>
                     <div className="w-2/3">
-                        <form name="contact" method="POST" data-netlify="true" onSubmit="submit" className="mb-6 space-y-5" data-netlify-honeypot="bot-field">
+                        <form name="contact" action="/success" method="POST" data-netlify="true" onSubmit="submit" className="mb-6 space-y-5" data-netlify-honeypot="bot-field">
                             <input name="bot-field" className="hidden" />
                             {/* <label for="name" class="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-300">Full Name : </label> */}
                             <input name="name" type="text" placeholder="Your full name" id="name" className="text-lg mt-0 bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
---
+++ pages/success.js
@@ -0,0 +1,7 @@
+const Success = () => {
+    return (
+        <div>Message has been sent</div>
+    );
+}
+
+export default Success;
