1e9ca67806d5da3808a78de67dfb3500f73a7eba
Faqih Madani <76164968+faqihmadani@users.noreply.github.com>
2024-02-17 23:42:59 +0700

    copyright

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="flex  border-t-2 border-gray-300 dark:border-gray-500 items-center dark:bg-slate-900 bg-slate-100 transition-color text-slate-900 dark:text-slate-100">
-            <p className="text-center w-full py-5">2024 - Faqih Madani</p>
+            <p className="text-center w-full py-5">&copy; 2024 Faqih Madani</p>
         </footer>
     );
 }
